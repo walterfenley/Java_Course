8c2c2723b5ad8b11131432892eb3850b0eda8001
Asghar Nazir <walterfenley@outlook.com>
2020-09-01 23:57:45 +0500

    2nd commit

--- src/com/Syntax/Main.java
+++ src/com/Syntax/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("first line added to the code");
+        System.out.println("second line added to the code");
     }
 }
